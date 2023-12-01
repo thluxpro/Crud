528603bcc63afbd54f0670ea7c43228474e33302
thluxpro <lucas_ezes099@hotmail.com>
2023-12-01 00:22:37 -0300

    primer commit y ultimo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmpleadosComponent } from './models/empleados/empleados.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practic';
+  empleadoarray: EmpleadosComponent[] = [
+    {id: 1, nombre:"Rodolfo" },
+    {id: 2, nombre:"Alberto" },
+    {id: 3, nombre:"Eustacio" },
+  ];
+  SelectedEmpleado: EmpleadosComponent= new EmpleadosComponent();
+  AbrirEditar(empleado: EmpleadosComponent){
+    this.SelectedEmpleado= empleado;
+  }
+  agregaroModificar(){
+    if (this.SelectedEmpleado.id === 0){
+      this.SelectedEmpleado.id = this.empleadoarray.length + 1;
+      this.empleadoarray.push(this.SelectedEmpleado);
+    }
+
+
+    this.SelectedEmpleado = new EmpleadosComponent();
+  }
+  eliminar(){
+    if (confirm('Esta seguro de eliminarlo?')){
+      this.empleadoarray = this.empleadoarray.filter(x => x != this.SelectedEmpleado);
+      this.SelectedEmpleado = new EmpleadosComponent();
+    }
+  }  
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpleadosComponent } from './models/empleados/empleados.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
